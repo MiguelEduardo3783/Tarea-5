b32deb15eb4ee814e18b241bab70ae9fa97b6957
MiguelEduardo3783 <miguel.eduardo01@hotmail.com>
2020-05-24 21:56:02 -0500

    Tarea 5

---
+++ src/app/page4/page4.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page4',
+  templateUrl: './page4.page.html',
+  styleUrls: ['./page4.page.scss'],
+})
+export class Page4Page implements OnInit {
+
+  IsPlaying = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  Sonido()
+  {
+      let Sound=new Audio();
+      Sound.src="assets/SSB4_S.mp3";
+      Sound.load();
+      Sound.play();
+  }
+
+}
